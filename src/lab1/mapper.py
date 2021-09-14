f7c2de53993a26cbd25ef815c0f050c0f3840c80
Tyler Haden <tylerjohnhaden@gmail.com>
2021-09-14 15:52:56 -0400

    update mapper

--- src/lab1/mapper.py
+++ src/lab1/mapper.py
@@ -38,37 +38,45 @@ def parse_message(m):
     return None, None, True
 
 
-def update_output(out, x_pts, y_pts):
+def topic_listener(topic):
+    popen = Popen(['rostopic', 'echo', topic], stdout=PIPE, universal_newlines=True, close_fds=ON_POSIX)
+    for stdout_line in iter(popen.stdout.readline, ''):
+        yield stdout_line
+    popen.stdout.close()
+
+def update_output(out):
+    global x_points
+    global y_points
+    print('here')
+
     for line in iter(out.readline, b''):
         x, y, err = parse_message(line)
         if not err:
-            x_pts.append(x)
-            y_pts.append(y)
+            x_points.append(x)
+            y_points.append(y)
     out.close()
 
 
 if __name__ == '__main__':
-    # Threading idea partly taken from https://stackoverflow.com/a/4896288
-    p = Popen(['rostopic', 'echo', topic], stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
-    t = Thread(target=update_output, args=(p.stdout, x_points, y_points))
-    t.daemon = True
-    t.start()
+    for thing in topic_listener(topic=topic):
+        print(f'>>{thing}<<')
+        print(parse_message(thing))
 
-    fig, ax = plt.subplots()
-    ln, = plt.scatter([], [], 'ro')
-
-
-    def init():
-        half_length = int(room_longest_side_length / 2)
-        ax.set_xlim(-half_length, half_length)
-        ax.set_ylim(-half_length, half_length)
-        return ln,
-
-
-    def update(frame):
-        ln.set_data(x_points, y_points)
-        return ln,
-
-
-    ani = FuncAnimation(fig, update, init_func=init, blit=True, interval=1000)
-    plt.show()
+    # fig, ax = plt.subplots()
+    # ln, = plt.scatter([], [], 'ro')
+    #
+    #
+    # def init():
+    #     half_length = int(room_longest_side_length / 2)
+    #     ax.set_xlim(-half_length, half_length)
+    #     ax.set_ylim(-half_length, half_length)
+    #     return ln,
+    #
+    #
+    # def update(frame):
+    #     ln.set_data(x_points, y_points)
+    #     return ln,
+    #
+    #
+    # ani = FuncAnimation(fig, update, init_func=init, blit=True, interval=1000)
+    # plt.show()
